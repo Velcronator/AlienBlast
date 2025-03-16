4d7ab8f5380ebadf7253d540a06e086199552f58
velcronator <valacos7@gmail.com>
2025-03-17 10:06:15 +1100

    Ladybug can turn around

--- Assets/Scripts/Ladybug.cs
+++ Assets/Scripts/Ladybug.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Ladybug : MonoBehaviour
@@ -28,24 +29,49 @@ public class Ladybug : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawLine(origin, origin + (_direction * _raycastDistance));
 
-        var bounds = collider.bounds;
+        var downOrigin = GetDownRayPosition(collider);
 
+        Gizmos.DrawLine(downOrigin, downOrigin + (Vector2.down * _raycastDistance));
+    }
+
+    Vector2 GetDownRayPosition(Collider2D collider)
+    {
+        var bounds = collider.bounds;
         if (_direction == Vector2.left)
         {
-            Vector2 bottomLeft = new Vector2(bounds.center.x - bounds.extents.x, bounds.center.y - bounds.extents.y);
-            Gizmos.DrawLine(bottomLeft, bottomLeft + (Vector2.down * _raycastDistance));
+            return new Vector2(bounds.center.x - bounds.extents.x, bounds.center.y - bounds.extents.y);
         }
         else
         {
-            Vector2 bottomRight = new Vector2(bounds.center.x + bounds.extents.x, bounds.center.y - bounds.extents.y);
-            Gizmos.DrawLine(bottomRight, bottomRight + (Vector2.down * _raycastDistance));
+            return new Vector2(bounds.center.x + bounds.extents.x, bounds.center.y - bounds.extents.y);
         }
     }
+
+
 
     void Update()
     {
         Vector2 offset = _direction * _collider.bounds.extents.x;
         Vector2 origin = (Vector2)transform.position + offset;
+
+        bool canContinueWalking = false;
+        var downOrigin = GetDownRayPosition(_collider);
+        var downHits = Physics2D.RaycastAll(downOrigin, Vector2.down, _raycastDistance);
+        foreach (var hit in downHits)
+        {
+            if (hit.collider != null && hit.collider.gameObject != gameObject)
+            {
+                canContinueWalking = true;
+                break;
+            }
+        }
+
+        if (canContinueWalking == false)
+        {
+            _direction *= -1;
+            _spriteRenderer.flipX = _direction == Vector2.right;
+            return;
+        }
 
         var hits = Physics2D.RaycastAll(origin, _direction, _raycastDistance);
         foreach (var hit in hits)
