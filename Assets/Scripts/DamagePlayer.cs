733196d14280a1674c693608dd789ad635193702
velcronator <valacos7@gmail.com>
2025-03-17 14:42:53 +1100

    26D IgnoreFromTop: Changed the spike.cs to DamagePlayer.cs and add if we ignore hits from above.

--- Assets/Scripts/DamagePlayer.cs
+++ Assets/Scripts/DamagePlayer.cs
@@ -1,9 +1,19 @@
 using UnityEngine;
 
-public class Spikes : MonoBehaviour
+public class DamagePlayer : MonoBehaviour
 {
+    [SerializeField] bool _ignoreFromTop;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
+        if (_ignoreFromTop)
+        {
+            Vector2 normal = collision.contacts[0].normal;
+            float dot = Vector2.Dot(normal, Vector2.down);
+            if (dot > 0.5)
+                return;
+        }
+
         if (collision.collider.CompareTag("Player"))
         {
             var player = collision.collider.GetComponent<Player>();
