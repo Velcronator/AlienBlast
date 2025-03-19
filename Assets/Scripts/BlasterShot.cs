dd98164c1bddbd52180d0daa49f37b5863bd80b9
velcronator <valacos7@gmail.com>
2025-03-20 00:08:28 +1100

    30A Blasting Dogs Only: Blastershot kills dog

--- Assets/Scripts/BlasterShot.cs
+++ Assets/Scripts/BlasterShot.cs
@@ -25,6 +25,11 @@ public class BlasterShot : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        var dog = collision.gameObject.GetComponent<Dog>();
+        if (dog != null)
+        {
+            dog.TakeDamage();
+        }
         gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/Dog.cs
+++ Assets/Scripts/Dog.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Dog : MonoBehaviour
@@ -5,5 +6,11 @@ public class Dog : MonoBehaviour
     public void Shoot()
     {
         Debug.Log("Dog shoots");
+    }
+
+    public void TakeDamage()
+    {
+        Debug.Log("Dog takes damage");
+        gameObject.SetActive(false);
     }
 }
