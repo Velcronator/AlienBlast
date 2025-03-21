b90d58d75d22d30fdb1df0dad7f1cf40c56d4e43
velcronator <valacos7@gmail.com>
2025-03-21 18:10:36 +1100

    32C Blaster Pool: Blaster shots are working with the pool

--- Assets/Scripts/Blaster.cs
+++ Assets/Scripts/Blaster.cs
@@ -1,25 +1,45 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Pool;
 
 public class Blaster : MonoBehaviour
 {
     [SerializeField] BlasterShot _blasterShotPrefab;
     [SerializeField] Transform _blasterShotSpawnPoint;
 
+    ObjectPool<BlasterShot> _pool;
     Player _player;
     PlayerInput _playerInput;
 
     void Awake()
     {
+        _pool = new ObjectPool<BlasterShot>(AddNewBlasterShotToPool,
+            t => t.gameObject.SetActive(true),
+            t => t.gameObject.SetActive(false));
         _player = GetComponent<Player>();
         _playerInput = GetComponent<PlayerInput>();
-        _playerInput.actions["Attack"].performed += TryAttack;
+        //_playerInput.actions["Attack"].performed += TryAttack;
+    }
+
+    BlasterShot AddNewBlasterShotToPool()
+    {
+        var shot = Instantiate(_blasterShotPrefab);
+        shot.SetPool(_pool);
+        return shot;
     }
 
     void TryAttack(InputAction.CallbackContext context)
     {
-        BlasterShot shot = Instantiate(_blasterShotPrefab, _blasterShotSpawnPoint.position, Quaternion.identity);
-        shot.Launch(_player.Direction);
+        BlasterShot shot = _pool.Get();// Instantiate(_blasterShotPrefab, _blasterShotSpawnPoint.position, Quaternion.identity);
+        shot.Launch(_player.Direction, _blasterShotSpawnPoint.position);
+    }
+
+    private void Update()
+    {
+        if (_playerInput.actions["Attack"].ReadValue<float>() > 0)
+        {
+            BlasterShot shot = _pool.Get();
+            shot.Launch(_player.Direction, _blasterShotSpawnPoint.position);
+        }
     }
 }
--- Assets/Scripts/BlasterShot.cs
+++ Assets/Scripts/BlasterShot.cs
@@ -1,27 +1,42 @@
+using System;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class BlasterShot : MonoBehaviour
 {
     [SerializeField] float _speed = 5f;
     [SerializeField] GameObject _impactExplosion;
+    [SerializeField] float _maxLifetime = 4f;
 
     Rigidbody2D _rb;
     Vector2 _direction = Vector2.right;
+    ObjectPool<BlasterShot> _pool;
+    float _selfDestructTime;
 
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
     }
 
+    void SelfDestruct()
+    {
+        _pool.Release(this);
+    }
+
     void Update()
     {
         _rb.linearVelocity = _direction * _speed;
+        if (Time.time > _selfDestructTime)
+            SelfDestruct();
     }
 
-    public void Launch(Vector2 direction)
+    public void Launch(Vector2 direction, Vector2 position)
     {
+        transform.position = position;
         _direction = direction;
         transform.rotation = _direction == Vector2.right ? Quaternion.identity : Quaternion.Euler(0, 180, 0);
+        _selfDestructTime = Time.time + _maxLifetime;
+        //Invoke(nameof(SelfDestruct), _maxLifetime);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -33,6 +48,11 @@ public class BlasterShot : MonoBehaviour
         var explosion = Instantiate(_impactExplosion, collision.contacts[0].point, Quaternion.identity);
         Destroy(explosion.gameObject, 0.9f);
 
-        gameObject.SetActive(false);
+        SelfDestruct();
+    }
+
+    public void SetPool(ObjectPool<BlasterShot> pool)
+    {
+        _pool = pool;
     }
 }
