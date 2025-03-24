cbf514d78e93205d3f92a42cd8e8cd4e6c56685d
velcronator <valacos7@gmail.com>
2025-03-24 23:52:35 +1100

    33A – Blaster Animation: changed the layer for attack

--- Assets/Scripts/Blaster.cs
+++ Assets/Scripts/Blaster.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Pool;
@@ -6,28 +7,33 @@ public class Blaster : MonoBehaviour
 {
     [SerializeField] Transform _blasterShotSpawnPoint;
 
+    Animator _animator;
     Player _player;
     PlayerInput _playerInput;
 
     void Awake()
     {
+        _animator = GetComponentInChildren<Animator>();
         _player = GetComponent<Player>();
         _playerInput = GetComponent<PlayerInput>();
-        //_playerInput.actions["Attack"].performed += TryAttack;
+        _playerInput.actions["Attack"].performed += TryAttack;
     }
 
     void TryAttack(InputAction.CallbackContext context)
     {
+        Attack();
+    }
+
+    private void Attack()
+    {
         BlasterShot shot = PoolManager.Instance.GetBlasterShot();
         shot.Launch(_player.Direction, _blasterShotSpawnPoint.position);
+        _animator.SetTrigger("Attack");
     }
 
     private void Update()
     {
-        if (_playerInput.actions["Attack"].ReadValue<float>() > 0)
-        {
-            BlasterShot shot = PoolManager.Instance.GetBlasterShot();
-            shot.Launch(_player.Direction, _blasterShotSpawnPoint.position);
-        }
+        //rapid fire
+        //if (_playerInput.actions["Attack"].ReadValue<float>() > 0) Attack();
     }
 }
