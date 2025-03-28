e373e6604a5ad6c9fb2f1b136a38adbd98e223a1
velcronator <valacos7@gmail.com>
2025-03-29 10:34:13 +1100

    34C Bad Key: It works, but less than optimal. As the Key script knows too much about the playerinput.

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Key : MonoBehaviour
 {
+    [SerializeField] float _useRange = 1f;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         var player = collision.GetComponent<Player>();
@@ -9,7 +12,22 @@ public class Key : MonoBehaviour
         {
             transform.SetParent(player.ItemPoint);
             transform.localPosition = Vector3.zero;
+            var playerInput = player.GetComponent<PlayerInput>();
+            playerInput.actions["Attack"].performed += UseKey;
         }
+    }
 
+    void UseKey(InputAction.CallbackContext obj)
+    {
+        var hits = Physics2D.OverlapCircleAll(transform.position, _useRange);
+        foreach (var hit in hits)
+        {
+            var toggleLock = hit.GetComponent<ToggleLock>();
+            if (toggleLock)
+            {
+                toggleLock.Toggle();
+                break;
+            }
+        }
     }
 }
