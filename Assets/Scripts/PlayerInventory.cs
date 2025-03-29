789438225502ae57ed2a860b5490f2262b162c1e
velcronator <valacos7@gmail.com>
2025-03-29 12:32:58 +1100

    34E Key Switching:

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -1,31 +1,61 @@
+using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerInventory : MonoBehaviour
 {
     PlayerInput _playerInput;
-    Key _equippedKey;
+    Key EquippedKey => _items.Count >= _currentItemIndex ? _items[_currentItemIndex] : null;
     public Transform ItemPoint;
+    List<Key> _items = new List<Key>();
+    int _currentItemIndex;
 
     void Awake()
     {
         _playerInput = GetComponent<PlayerInput>();
         _playerInput.actions["Attack"].performed += UseEquippedItem;
+        _playerInput.actions["Next"].performed += EquipNext;
+        _playerInput.actions["Previous"].performed += EquipPrevious;
+    }
+
+    private void EquipPrevious(InputAction.CallbackContext context)
+    {
+        _currentItemIndex--;
+        if (_currentItemIndex < 0)
+            _currentItemIndex = _items.Count - 1;
+        ToggleEquippedItem();
+    }
+
+    void EquipNext(InputAction.CallbackContext context)
+    {
+        _currentItemIndex++;
+        if (_currentItemIndex >= _items.Count)
+            _currentItemIndex = 0;
+        ToggleEquippedItem();
+    }
+
+    private void ToggleEquippedItem()
+    {
+        for (int i = 0; i < _items.Count; i++)
+        {
+            _items[i].gameObject.SetActive(i == _currentItemIndex);
+        }
     }
 
     void UseEquippedItem(InputAction.CallbackContext context)
     {
-        if (_equippedKey)
-            _equippedKey.Use();
+        if (EquippedKey)
+            EquippedKey.Use();
     }
 
     public void Pickup(Key key)
     {
         key.transform.SetParent(ItemPoint);
         key.transform.localPosition = Vector3.zero;
-        _equippedKey = key;
+        _items.Add(key);
+        _currentItemIndex = _items.Count - 1;
+        ToggleEquippedItem();
     }
-
-
 }
